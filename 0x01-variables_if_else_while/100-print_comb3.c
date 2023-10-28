c483048bd3e59a70333491202f4506ec1a62d84e
Bishopaul <monitepaul@gmail.com>
2023-10-27 21:44:35 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations of two digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a < 57; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			if (a > b)
+				continue;
+
+			else
+			{
+				putchar(a);
+				putchar(b);
+
+				if (a == 56 && b == 57)
+					break;
+
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations of three digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 56; a++)
+	{
+		for (b = 49; b < 57; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (a > b && b > c)
+					break;
+
+				else
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a == 55 && b == 56 && c == 57)
+						continue;
+					else
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
