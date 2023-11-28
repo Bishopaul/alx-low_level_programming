fcad4df30f4eec8ed51f07649f986a5ed5f1938a
Bishopaul <monitepaul@gmail.com>
2023-11-27 20:29:02 -0800

    Preprocessor

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -4,9 +4,9 @@
 #define PI 3.14159265359
 
 /**
- *  * Main - header file that defines a macro named PI
- *   *
- *    * Return: 0
- *     */
+ * Main - header file that defines a macro named PI
+ *
+ * Return: 0
+ */
 
 #endif
